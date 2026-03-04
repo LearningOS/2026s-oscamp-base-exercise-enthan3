e47ec4694257dc36f9d7b856d76c244478abd35f
hupeilu <p27hu@uwaterloo.ca>
2026-03-03 21:59:12 -0800

    Finished named_sleeper

--- exercises/01_concurrency_sync/01_thread_spawn/src/lib.rs
+++ exercises/01_concurrency_sync/01_thread_spawn/src/lib.rs
@@ -206,7 +206,11 @@ pub fn named_sleeper(value: i32, ms: u64) -> i32 {
     // TODO: Create a thread builder with name "sleeper"
     // TODO: Spawn a thread that sleeps for `ms` milliseconds and returns `value`
     // TODO: Join the thread and return the value
-    todo!()
+    let handle=thread::Builder::new().name("sleeper".to_string()).spawn(move || {
+        thread::sleep(Duration::from_millis(ms));
+        value
+    }).unwrap();
+    handle.join().unwrap()
 }
 
 thread_local! {
